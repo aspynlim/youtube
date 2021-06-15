52d5f6ac018f7e66dd6fc3cc9ab02792edec2462
aspynlim <aspyn.lim@gmail.com>
2021-06-15 15:25:09 +0900

    #4 Object & Arrays and #5 Explicit Types

--- net_ninja/1_typescript/sandBox.js
+++ net_ninja/1_typescript/sandBox.js
@@ -1,3 +1,4 @@
+// #3 Type Basics
 var inputs = document.querySelectorAll('input');
 inputs.forEach(function (input) {
     console.log(input);
@@ -8,3 +9,38 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(100));
+// #4 Object & Arrays
+var names = ['purple', 'apple'];
+names.push('test');
+console.log(names);
+var mixed = [20, 'test'];
+mixed.push(30);
+console.log(mixed);
+var ninja = {
+    name: 'mario',
+    age: 20,
+    skills: ['skiing', 'driving']
+};
+console.log(ninja);
+// #5 Explicit Types
+var company;
+company = 'test';
+company = false;
+console.log(company);
+var ninjas_mixed = [];
+ninjas_mixed.push('stay');
+ninjas_mixed.push(10);
+console.log(ninjas_mixed);
+var obj;
+obj = {
+    name: 'sam',
+    age: 20
+};
+obj = [20];
+console.log(obj);
+var obj2;
+obj2 = {
+    name: 'jo',
+    age: 30
+};
+console.log(obj2);
--- net_ninja/1_typescript/sandBox.ts
+++ net_ninja/1_typescript/sandBox.ts
@@ -1,3 +1,4 @@
+// #3 Type Basics
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
@@ -12,3 +13,51 @@ const circ = (diameter: number) => {
 };
 
 console.log(circ(100));
+
+
+// #4 Object & Arrays
+let names = ['purple', 'apple'];
+names.push('test');
+console.log(names);
+
+let mixed = [20, 'test'];
+mixed.push(30);
+console.log(mixed);
+
+let ninja = {
+  name: 'mario',
+  age: 20,
+  skills: ['skiing', 'driving']
+}
+
+console.log(ninja);
+
+
+// #5 Explicit Types
+let company: string|boolean;
+company = 'test';
+company = false;
+console.log(company);
+
+let ninjas_mixed: (string|number)[] = [];
+ninjas_mixed.push('stay');
+ninjas_mixed.push(10);
+console.log(ninjas_mixed);
+
+let obj: object;
+obj = {
+  name: 'sam',
+  age: 20
+}
+obj = [20];
+console.log(obj);
+
+let obj2: {
+  name: string,
+  age: number
+}
+obj2 = {
+  name: 'jo',
+  age: 30
+}
+console.log(obj2);
