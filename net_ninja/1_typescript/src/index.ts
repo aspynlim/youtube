99b859730703f60d05d689bab22762d25475555d
aspynlim <aspyn.lim@gmail.com>
2021-06-16 18:11:30 +0900

    #15 Interfaces

--- net_ninja/1_typescript/src/index.ts
+++ net_ninja/1_typescript/src/index.ts
@@ -1,5 +1,7 @@
 import { formData } from './forms';
+import { IsPerson } from './interfaces/isPerson';
 
+// formData
 const form = document.querySelector('form')!;
 
 form.addEventListener('submit', (e) => {
@@ -9,4 +11,22 @@ form.addEventListener('submit', (e) => {
 });
 
 const person: any = {};
-console.log(person.speak());
+// console.log(person.speak());
+
+// Interfaces > isPerson
+const me: IsPerson = {
+  name: 'jay',
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log(`I spent ${amount}.`);
+    return amount;
+  }
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log(`hello, ${person.name}`);
+}
+greetPerson(me);
---
+++ net_ninja/1_typescript/src/interfaces/isPerson.ts
@@ -0,0 +1,6 @@
+export interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
