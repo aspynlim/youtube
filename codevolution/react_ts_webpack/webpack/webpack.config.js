3d31bc374bbf89b66906575edf1f2d9953a0e8be
JenLim <cmdlhz@gmail.com>
2021-06-19 19:08:20 +0900

    React TypeScript Webpack - Setup From Scratch (1/8)

---
+++ codevolution/react_ts_webpack/webpack/webpack.config.js
@@ -0,0 +1,33 @@
+// To create an absolute path
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', './build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    })
+  ]
+}
