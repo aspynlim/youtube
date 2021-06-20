501ec985faf3f641c1eba5944f787e9384cf5277
JenLim <cmdlhz@gmail.com>
2021-06-20 21:46:41 +0900

    React TypeScript Webpack - Setup From Scratch (3/8) : dev & prod environents

--- codevolution/react_ts_webpack/src/App.tsx
+++ codevolution/react_ts_webpack/src/App.tsx
@@ -4,7 +4,7 @@ import bird from './bird.svg';
 
 export const App = () => {
   return <>
-  <h1>HELLO! Just do it!</h1>
+  <h1>HELLO! Just do it! {process.env.NODE_ENV} {process.env.name}</h1>
   <img src={image} alt="React Logo" width="200" />
   <img src={bird} alt="Bird" width="200" />
   </>
