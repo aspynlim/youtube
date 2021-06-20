4b2a05acf70dc5916ca116bb66a7ca2a28dceb2f
JenLim <cmdlhz@gmail.com>
2021-06-20 22:24:14 +0900

    React TypeScript Webpack - Setup From Scratch (4/8) : React Refresh Webpack Plugin

--- codevolution/react_ts_webpack/src/App.tsx
+++ codevolution/react_ts_webpack/src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.scss';
 import image from './react.png';
 import bird from './bird.svg';
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
   return <>
-  <h1>HELLO! Just do it! {process.env.NODE_ENV} {process.env.name}</h1>
+  <h1>HELLO! 123 Just do it! {process.env.NODE_ENV} {process.env.name}</h1>
   <img src={image} alt="React Logo" width="200" />
   <img src={bird} alt="Bird" width="200" />
+  < ClickCounter />
   </>
 }
