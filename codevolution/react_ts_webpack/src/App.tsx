f8c5b91f4746247d1f8636763e8bcf05dcf52564
JenLim <cmdlhz@gmail.com>
2021-06-20 15:25:31 +0900

    React TypeScript Webpack - Setup From Scratch (2/8) (+ Image upload : png, svg)

--- codevolution/react_ts_webpack/src/App.tsx
+++ codevolution/react_ts_webpack/src/App.tsx
@@ -1,5 +1,11 @@
 import './styles.scss';
+import image from './react.png';
+import bird from './bird.svg';
 
 export const App = () => {
-  return <h1>HELLO! Just do it!</h1>
+  return <>
+  <h1>HELLO! Just do it!</h1>
+  <img src={image} alt="React Logo" width="200" />
+  <img src={bird} alt="Bird" width="200" />
+  </>
 }
