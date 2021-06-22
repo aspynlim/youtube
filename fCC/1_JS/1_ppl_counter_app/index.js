9682ddedb45917c14318aa43c9622ccbbc2c9847
aspynlim <aspyn.lim@gmail.com>
2021-06-22 16:13:16 +0900

    #1 Passenger Counter App: Create variables

--- fCC/1_JS/1_ppl_counter_app/index.js
+++ fCC/1_JS/1_ppl_counter_app/index.js
@@ -1,1 +1,8 @@
-document.getElementById('count-el').innerText = 6;
+// document.getElementById('count-el').innerText = 6;
+
+let count = 0;
+console.log(count);
+
+// Uncaught ReferenceError: Cannot access 'myAge' before initialization
+console.log(myAge);
+let myAge = 32;
