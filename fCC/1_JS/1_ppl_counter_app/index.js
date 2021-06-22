0f3e47c7674768c5783aa3dd0eadd4b9557cee8e
aspynlim <aspyn.lim@gmail.com>
2021-06-22 17:34:57 +0900

    #1 Passenger Counter App: Create save button

--- fCC/1_JS/1_ppl_counter_app/index.js
+++ fCC/1_JS/1_ppl_counter_app/index.js
@@ -3,6 +3,10 @@ let count = 0;
 let num = document.getElementById('count-el');
 let btn = document.getElementById('increment-btn');
 
+function save() {
+  console.log(count);
+}
+
 function increment(){
   btn.addEventListener('click', () => {
     count += 1;
