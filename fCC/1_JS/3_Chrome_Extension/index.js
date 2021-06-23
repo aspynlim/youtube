da0150c4355e766417930b4419871efedd17920e
aspynlim <aspyn.lim@gmail.com>
2021-06-23 18:43:41 +0900

    #3 Chrome Extension : Basic Setup (including innerHTML)

---
+++ fCC/1_JS/3_Chrome_Extension/index.js
@@ -0,0 +1,22 @@
+let myLeads = ['red', 'blue'];
+const inputEl = document.querySelector('#input-el');
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.querySelector('#ul-el');
+
+// Uncaught TypeError: Assignment to constant variable.
+// inputEl = 'test';
+
+inputBtn.addEventListener('click', () => {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
+
+for(let lead of myLeads){
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(lead));
+  ulEl.appendChild(li);
+}
+
+for(let lead of myLeads){
+  ulEl.innerHTML += `<li> ${lead} </li>`;
+}
