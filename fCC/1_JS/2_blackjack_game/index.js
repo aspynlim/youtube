b1145531351cf3c39dca02de276bfcfc5ae9ea0d
aspynlim <aspyn.lim@gmail.com>
2021-06-23 11:48:43 +0900

    #2 Build Blackjack Game: New card button

--- fCC/1_JS/2_blackjack_game/index.js
+++ fCC/1_JS/2_blackjack_game/index.js
@@ -1,14 +1,19 @@
 let firstCard = 10;
-let secondCard = 31;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let msg = '';
+let messageEl = document.querySelector('#message-el');
+let cardsEl = document.querySelector('#cards-el');
+let sumEl = document.querySelector('#sum-el');
 
 console.log(firstCard, secondCard, sum);
 
-
 function startGame(){
+  sumEl.textContent = `Sum : ${sum}`;
+  cardsEl.textContent = `Cards : ${firstCard} ${secondCard}`;
+
   if(sum <= 20){
     msg = "Do you want to draw a new card ?";
   } else if(sum === 21) {
@@ -19,7 +24,12 @@ function startGame(){
     isAlive = false;
   }
 
-  console.log(msg);
+  messageEl.textContent = msg;
   console.log("hasBlackJack", hasBlackJack);
   console.log("isAlive", isAlive);
 }
+
+function newCard(){
+  console.log(`new card!`);
+}
+
