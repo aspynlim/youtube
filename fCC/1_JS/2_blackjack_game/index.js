9a42b536fa939a0703a285e0a3bffe131aa9aab4
aspynlim <aspyn.lim@gmail.com>
2021-06-23 11:22:33 +0900

    #2 Build Blackjack Game: Make start button work

--- fCC/1_JS/2_blackjack_game/index.js
+++ fCC/1_JS/2_blackjack_game/index.js
@@ -1,5 +1,25 @@
-let firstCard = Math.floor(Math.random() * 10);
-let secondCard = Math.floor(Math.random() * 10);
+let firstCard = 10;
+let secondCard = 31;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let msg = '';
 
-console.log(num1, num2, sum);
+console.log(firstCard, secondCard, sum);
+
+
+function startGame(){
+  if(sum <= 20){
+    msg = "Do you want to draw a new card ?";
+  } else if(sum === 21) {
+    msg = "WIN!";
+    hasBlackJack = true;
+  } else {
+    msg = "Lost...";
+    isAlive = false;
+  }
+
+  console.log(msg);
+  console.log("hasBlackJack", hasBlackJack);
+  console.log("isAlive", isAlive);
+}
