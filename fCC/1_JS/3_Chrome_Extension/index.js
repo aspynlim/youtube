deb76a46d3897d3e5de1698a1efe445b8a15ff38
aspynlim <aspyn.lim@gmail.com>
2021-06-24 11:10:55 +0900

    #3 Chrome Extension : Refactoring code + list styling

--- fCC/1_JS/3_Chrome_Extension/index.js
+++ fCC/1_JS/3_Chrome_Extension/index.js
@@ -1,22 +1,32 @@
-let myLeads = ['red', 'blue'];
-const inputEl = document.querySelector('#input-el');
-const inputBtn = document.getElementById('input-btn');
-const ulEl = document.querySelector('#ul-el');
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.querySelector("#ul-el");
+
+inputBtn.addEventListener("click", () => {
+  myLeads.push(inputEl.value);
+  console.log(inputEl.value);
+  renderLeads();
+  inputEl.value = "";
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let lead of myLeads) {
+    // Use "Template String"
+    listItems += `
+      <li>
+        <a href="${lead}" target="_blank">${lead}</a>
+      </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
 
 // Uncaught TypeError: Assignment to constant variable.
 // inputEl = 'test';
 
-inputBtn.addEventListener('click', () => {
-  myLeads.push(inputEl.value);
-  console.log(myLeads);
-});
-
-for(let lead of myLeads){
-  const li = document.createElement("li");
-  li.textContent = lead;
-  ulEl.append(li);
-}
-
-for(let lead of myLeads){
-  ulEl.innerHTML += `<li> ${lead} </li>`;
-}
+// for(let lead of myLeads){
+//   const li = document.createElement("li");
+//   li.textContent = lead;
+//   ulEl.append(li);
+// }
