f90fb8f009873bf500f82d70dd5050879cf64ceb
aspynlim <aspyn.lim@gmail.com>
2021-06-25 11:19:11 +0900

    React 2021 Academind: Pros & Dynamic Content + Handling Events + Adding More Components

---
+++ academind/1_react_2021/src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div className="modal">
+      <p>Are you Sure?</p>
+      <button className="btn btn--alt">CANCEL</button>
+      <button className="btn">CONFIRM</button>
+    </div>
+  );
+}
+
+export default Modal;
